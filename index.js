254c36ff79e5aa71fb52ccde3fda02e09c56f67c
chennick <chennick@users.noreply.github.com>
2017-10-29 04:02:42 +0800

    TEST
    TEST

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 function clickCheckbox(element){
-  if(element.checked = true){
-  $('#button1').attr('style','block');
-    $("#button1").show();
-  }else{
-    $("#button1").hide();
-  }
+  $("#button1").show();
 }
